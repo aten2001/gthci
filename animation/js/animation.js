1c168b07ac955ff3e0a3900f76634563467fec5d
zager00 <estherleeyuling@gmail.com>
2014-04-02 17:45:52 -0400

    Added animation stuff, it's still really buggy

---
+++ animation/js/animation.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+alert('loaded');
+
+	$('#custom').click(function() {
+	  alert('customed clicked');
+	});
+
+	$('#circle').click(function(){
+    alert('hi');
+});
+	
+	
+}

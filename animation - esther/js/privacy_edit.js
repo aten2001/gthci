0a160ccced5d9d94a78ae4d6866b347c13ef0129
zager00 <estherleeyuling@gmail.com>
2014-04-04 23:25:38 -0400

    sequence done

--- animation - esther/js/privacy_edit.js
+++ animation - esther/js/privacy_edit.js
@@ -13,31 +13,6 @@ $(document).ready(function(){
 		window.location = "quick.html";
 	});
 	
-	$('#done2').click(function() {
-		$('.indexPageID').fadeTo( "slow", 0.0);
-		window.location = "privacy2.html";
-	});
-	
-	$('#done').click(function() {
-		$('.indexPageID').fadeTo( "slow", 0.0);
-		window.location = "privacy.html";
-	});
-	
-	$('#shortcuts').click(function() {
-		$('.indexPageID').fadeTo( "slow", 0.0);
-		window.location = "home.html";
-	});
-	
-	$('#shortcuts2').click(function() {
-		$('.indexPageID').fadeTo( "slow", 0.0);
-		window.location = "home2.html";
-	});
-	
-	$('#PrivacyEye').click(function() {
-		
-		window.location = "privacy2.html";
-	});
-	
 	$('#settings1ID').bind('change', function () {
           var url = $(this).val(); // get selected value
           if (url) { // require a URL
@@ -45,18 +20,19 @@ $(document).ready(function(){
           }
           return false;
       });
-	  
+	  /*
 	$(window).resize(function() {
 		console.log("window resized");
 		$('#defHeader1ID').width($('#indexHeaderID').width());
 		$('#backDivID').width('20%');
 		$('#nameDivID').width('20%');
 	});
+	*/
 	
-	$('.ui-li-heading').click(function() {
-	alert("item clicked");
+
 	
 	
-	});
+	
+	
 	
 });
